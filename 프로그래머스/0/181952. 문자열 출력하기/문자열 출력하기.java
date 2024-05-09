4a8b987ba76394ff2abb7c8317d186eecbc89de1
iam0chan <143627721+iam0chan@users.noreply.github.com>
2024-05-09 20:33:38 +0900

    [level 0] Title: 문자열 출력하기, Time: 126.98 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int a = str.length();
+        if(a>=1 && a<= 1000000){
+            System.out.println(str);
+        }
+    }
+}
