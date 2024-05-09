f42fdfd4a145f57e6b79d4839b586b88b5447652
iam0chan <143627721+iam0chan@users.noreply.github.com>
2024-05-09 20:29:48 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int cnt = num_list.length-1;
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = num_list[cnt--];
+        }
+        return answer;
+    }
+}
