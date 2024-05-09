e8a756c5895cb8bf8a681ffbc7d116c9a3446227
iam0chan <143627721+iam0chan@users.noreply.github.com>
2024-05-09 20:20:39 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        boolean check = num1>0 && num1<=100 && num2>0 && num2<=100;
+        if(check){
+            answer = num1%num2;
+        }
+        return answer;
+    }
+}
