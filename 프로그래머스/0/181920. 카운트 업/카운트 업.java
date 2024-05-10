b37ea3364c4208b22451db94144a316cfe20670a
iam0chan <143627721+iam0chan@users.noreply.github.com>
2024-05-10 13:22:01 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = {};
+        answer = new int[(end_num-start_num)+1];
+        for(int i = 0; i<answer.length; i++){
+            answer[i] = start_num++;
+            if(end_num < start_num) break;
+        }
+        return answer;
+    }
+}
