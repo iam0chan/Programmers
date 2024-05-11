dc2529ceca4dc54c221e8d3719a3c7385927f4a4
iam0chan <143627721+iam0chan@users.noreply.github.com>
2024-05-11 14:11:43 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.16 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,10 +1,15 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        answer = Integer.parseInt(a+""+b);
-        if(Integer.parseInt(b+""+a)>answer){
-            answer = Integer.parseInt(b+""+a);
+        // answer = Integer.parseInt(a+""+b);
+        String convertIntegerToString = String.valueOf(a)+String.valueOf(b);
+        answer = Integer.parseInt(convertIntegerToString);
+        if(Integer.parseInt(String.valueOf(b)+String.valueOf(a))> answer){
+            answer = Integer.parseInt(String.valueOf(b)+String.valueOf(a));
         }
+        // if(Integer.parseInt(b+""+a)>answer){
+        //     answer = Integer.parseInt(b+""+a);
+        // }
         return answer;
     }
 }
